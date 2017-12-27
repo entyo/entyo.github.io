02e7101e3e43e28c429622625e9eb591ddf45e9d
e_ntyo <entyo.contact@gmail.com>
2017-12-27 13:36:37 +0900

    [WIP] browsersyncを使う, releaseへpush -> masterへpush(自動デプロイ)をやる

--- build.sh
+++
@@ -1,7 +0,0 @@
-webpack
-TMP=/tmp/`date '+%Y%m%d-%H%M%S'`
-mkdir $TMP && \
-cp -r index.html assets dist $TMP && \
-git checkout master && \
-rm -rf ./* && \
-cp -r $TMP/* .
--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -18,38 +18,38 @@ then
 fi
 
 # make a directory to put the gp-pages branch
-mkdir gh-pages-branch
-cd gh-pages-branch
-# now lets setup a new repo so we can update the gh-pages branch
+mkdir master-branch
+cd master-branch
+# now lets setup a new repo so we can update the master branch
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 git init
 git remote add --fetch origin "$remote"
 
-# switch into the the gh-pages branch
-if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
+# switch into the the master branch
+if git rev-parse --verify origin/master > /dev/null 2>&1
 then
-    git checkout gh-pages
+    git checkout master
     # delete any old site as we are going to replace it
     # Note: this explodes if there aren't any, so moving it here for now
     git rm -rf .
 else
-    git checkout --orphan gh-pages
+    git checkout --orphan master
 fi
 
 # copy over or recompile the new site
-cp -a "../${siteSource}/." .
+cp -a ${siteSource}/* .
 
 # stage any changes and new files
 git add -A
-# now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
+# now commit, ignoring branch master doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages > /dev/null 2>&1
+git push --force --quiet origin master > /dev/null 2>&1
 
-# go back to where we started and remove the gh-pages git repo we made and used
+# go back to where we started and remove the master git repo we made and used
 # for deployment
 cd ..
-rm -rf gh-pages-branch
+rm -rf master-branch
 
-echo "Finished Deployment!"
+echo "Finished Deployment!"\\
