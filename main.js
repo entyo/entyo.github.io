ef1b89070d8534ab73e104576546cc7fb96735af
entyo-machine <e_ntyo@softbank.ne.jp>
2018-05-12 15:49:18 +0000

    Deploy to GitHub pages [ci skip]

--- main.js
+++ main.js
@@ -201,7 +201,7 @@ var AppModule = /** @class */ (function () {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<video src=\"../../assets/otaku.mp4\" #origin></video>\n<div #rendererContainer></div>"
+module.exports = "<video src=\"../../assets/otaku.mp4\" id=\"origin\"></video>\n<div #rendererContainer></div>"
 
 /***/ }),
 
@@ -308,22 +308,24 @@ var BackgroundComponent = /** @class */ (function () {
         this.renderer = new three__WEBPACK_IMPORTED_MODULE_1__["WebGLRenderer"]();
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
-        this.videoOrigin.nativeElement.autoplay = true;
-        this.videoOrigin.nativeElement.crossOrigin = 'anonymous';
-        this.videoOrigin.nativeElement.loop = true;
+        // It must exist
+        this.videoOrigin = document.getElementById('origin');
+        console.log(this.videoOrigin);
+        this.videoOrigin.crossOrigin = 'anonymous';
+        this.videoOrigin.loop = true;
         (function () { return __awaiter(_this, void 0, void 0, function () {
             var vw, vh, videoMaterial, planeGeometry, plane, copyPass;
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4 /*yield*/, this.videoOrigin.nativeElement.play()];
+                    case 0: return [4 /*yield*/, this.videoOrigin.play()];
                     case 1:
                         _a.sent();
-                        this.vTexture = new three__WEBPACK_IMPORTED_MODULE_1__["Texture"](this.videoOrigin.nativeElement);
+                        this.vTexture = new three__WEBPACK_IMPORTED_MODULE_1__["Texture"](this.videoOrigin);
                         this.vTexture.minFilter = three__WEBPACK_IMPORTED_MODULE_1__["LinearFilter"];
                         this.vTexture.magFilter = three__WEBPACK_IMPORTED_MODULE_1__["LinearFilter"];
                         this.vTexture.format = three__WEBPACK_IMPORTED_MODULE_1__["RGBFormat"];
-                        vw = this.videoOrigin.nativeElement.videoWidth;
-                        vh = this.videoOrigin.nativeElement.videoHeight;
+                        vw = this.videoOrigin.videoWidth;
+                        vh = this.videoOrigin.videoHeight;
                         videoMaterial = new three__WEBPACK_IMPORTED_MODULE_1__["MeshBasicMaterial"]({
                             map: this.vTexture
                         });
@@ -347,7 +349,7 @@ var BackgroundComponent = /** @class */ (function () {
         }); })();
     };
     BackgroundComponent.prototype.render = function () {
-        if (this.videoOrigin.nativeElement.readyState === this.videoOrigin.nativeElement.HAVE_ENOUGH_DATA) {
+        if (this.videoOrigin.readyState === this.videoOrigin.HAVE_ENOUGH_DATA) {
             if (this.vTexture)
                 this.vTexture.needsUpdate = true;
         }
@@ -360,10 +362,6 @@ var BackgroundComponent = /** @class */ (function () {
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["ViewChild"])('rendererContainer'),
         __metadata("design:type", _angular_core__WEBPACK_IMPORTED_MODULE_0__["ElementRef"])
     ], BackgroundComponent.prototype, "rendererContainer", void 0);
-    __decorate([
-        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["ViewChild"])('origin'),
-        __metadata("design:type", _angular_core__WEBPACK_IMPORTED_MODULE_0__["ElementRef"])
-    ], BackgroundComponent.prototype, "videoOrigin", void 0);
     BackgroundComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
             selector: 'app-background',
